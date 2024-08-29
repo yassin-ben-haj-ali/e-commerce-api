4a65b31e6dd7b2d66a3043b08636e1b996b45fe7
yassin-ben-haj-ali <yassinbenhajali5@gmail.com>
2024-08-29 18:05:18 +0100

    adding address table

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express"
 import { ErrorCode, HttpException } from "./exceptions/root"
 import { InternalException } from "./exceptions/internal-exception"
+import { ZodError } from "zod"
+import { UnprocessableEntity } from "./exceptions/validation"
 
 export const errorHandler = (fn: Function) => {
-    return async(req: Request, res: Response, next: NextFunction) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
 
         try {
 
@@ -14,6 +16,9 @@ export const errorHandler = (fn: Function) => {
             if (err instanceof HttpException) {
                 exception = err
             } else {
+                if (err instanceof ZodError) {
+                    exception = new UnprocessableEntity(err.issues, 'Unprocessable entity', ErrorCode.UNPROCESSABLE_ENTITY)
+                }
                 exception = new InternalException('Something went wrong !', err, ErrorCode.INTERNAL_EXCEPTION)
             }
             next(exception)
